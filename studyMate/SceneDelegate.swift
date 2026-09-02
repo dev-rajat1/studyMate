b4c66e9a2b8a7f26a891d362b5da87ca16bdb963
dev-rajat1 <rajatdiamondn@gmail.com>
2026-09-02 15:20:09 +0530

    fix: remove UITabBar scrollEdgeAppearance from SceneDelegate for universal iOS compatibility

--- studyMate/SceneDelegate.swift
+++ studyMate/SceneDelegate.swift
@@ -3,7 +3,7 @@
 //  studyMate
 //
 //  Created for StudyMate AI.
-//  Purpose: Configures root window, programmatic TabBarController & Navigation hierarchy with modern iOS 18 glass styling.
+//  Purpose: Configures root window, programmatic TabBarController & Navigation hierarchy.
 //
 
 import UIKit
@@ -22,9 +22,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let window = UIWindow(windowScene: windowScene)
         self.window = window
         
-        // Apply Global Modern Appearance
-        setupGlobalAppearance()
-        
         // Build Programmatic Root TabBarController
         let tabBarController = createRootTabBarController()
         window.rootViewController = tabBarController
@@ -32,26 +29,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         // Apply saved theme (System / Light / Dark)
         UserDefaultsManager.shared.applyTheme()
-    }
-
-    // MARK: - Global Appearance Setup
-    private func setupGlobalAppearance() {
-        // Navigation Bar Appearance
-        let navBarAppearance = UINavigationBarAppearance()
-        navBarAppearance.configureWithDefaultBackground()
-        navBarAppearance.shadowColor = UIColor.separator.withAlphaComponent(0.2)
-        
-        UINavigationBar.appearance().standardAppearance = navBarAppearance
-        UINavigationBar.appearance().compactAppearance = navBarAppearance
-        UINavigationBar.appearance().tintColor = .systemPurple
-        
-        // Tab Bar Appearance
-        let tabBarAppearance = UITabBarAppearance()
-        tabBarAppearance.configureWithDefaultBackground()
-        tabBarAppearance.shadowColor = UIColor.separator.withAlphaComponent(0.2)
-        
-        UITabBar.appearance().standardAppearance = tabBarAppearance
-        UITabBar.appearance().tintColor = .systemPurple
     }
 
     // MARK: - Programmatic TabBar Hierarchy Setup
@@ -84,14 +61,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         tabBarController.selectedIndex = 1 // Default to Courses tab
         tabBarController.tabBar.tintColor = .systemPurple
         
-        if #available(iOS 15.0, *) {
-            let tabBarAppearance = UITabBarAppearance()
-            tabBarAppearance.configureWithDefaultBackground()
-            tabBarAppearance.shadowColor = UIColor.separator.withAlphaComponent(0.2)
-            tabBarController.tabBar.scrollEdgeAppearance = tabBarAppearance
-            tabBarController.tabBar.standardAppearance = tabBarAppearance
-        }
-        
         return tabBarController
     }
 
@@ -99,5 +68,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         CoreDataManager.shared.saveContext()
     }
 }
+
 
 
